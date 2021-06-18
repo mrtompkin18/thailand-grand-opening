8c34a0c81f6f7dcc4e03ad998607c49623271aa9
kittipong18 <kittipong@orcsoft.co.th>
2021-06-19 01:27:55 +0700

    test

--- component/Header.js
+++ component/Header.js
@@ -1,25 +1,22 @@
+import { memo } from "react";
 import Head from 'next/head';
-import { decode } from 'js-base64';
 
 function Header({ title, description, image, link, type }) {
     return (
         <Head>
-            {/* METAS */}
             <title>{title}</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
             <meta name="description" content={description}></meta>
             <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"></link>
-
-            {/* FACEBOOK */}
             <meta property="og:url" content={link} />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content={decode(image)} />
+            <meta property="og:image" content={`${link}/lungtoo.jpg`} />
             <meta property="og:type" content={type} />
             <meta property="og:locale" content="es_ES" />
             <meta property="og:site_name" content="xaconi.dev" />
         </Head>
     );
 }
-export default Header;
+export default memo(Header);
